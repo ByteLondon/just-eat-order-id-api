ee0126b76ffe6219b4ade4364dd9f170cf80d85d
Neats29 <anita.amini@protonmail.com>
2017-07-05 14:37:28 +0100

    Added the main Axios API function and the types

---
+++ src/facebook/types.ts
@@ -0,0 +1,56 @@
+//To fetch data
+interface OrderIdAttributions {
+  since: number //unix timestamp
+  until: number //unix timestamp
+  pixel_id: string
+  app_id: string
+  fields?: string
+  limit?: number
+}
+
+// Results we get back
+interface Results {
+  data: Fields[]
+  paging: {
+    cursors: {
+      before: string
+      after: string
+    }
+    next: string
+  }
+}
+
+interface Fields {
+  order_id: string
+  pixel_id: string
+  app_id: string
+  conversion_device:
+    | 'Android Web'
+    | 'Android Native'
+    | 'Desktop Web'
+    | 'iOS Web'
+    | 'iOS Native'
+    | 'Other'
+  order_timestamp: number //UTC timestamp
+  attributions: Attributions[]
+  attribution_type: 'link_click' | 'view'
+}
+
+interface Attributions {
+  ad_id: string
+  adset_id: string
+  campaign_id: string
+  account_id: string
+  action_type:
+    | 'Link Click'
+    | 'Social Click'
+    | 'Video Play'
+    | 'Other Click'
+    | 'Impression'
+  impression_cost: number //US Dollars
+  click_cost: number //US Dollars
+  impression_timestamp: number //UTC timestamp
+  click_timestamp: number //UTC timestamp
+  placement: 'WebFeed' | 'MobileFeed' | 'RHC' | 'Instagram' | 'Others'
+  device: 'Desktop' | 'Android' | 'iOS' | 'Others'
+}
---
+++ src/facebook/util/api.ts
@@ -0,0 +1,15 @@
+// https://graph.facebook.com/v2.9/<BUSINESS_ID>/order_id_attributions
+
+import axios, { AxiosResponse } from 'axios'
+
+export const checkStatusCode = <T>(response: AxiosResponse): Promise<T> => {
+  if (Math.floor(response.status / 100) !== 2) {
+    throw new Error(response.statusText)
+  }
+  return response.data
+}
+
+export default axios.create({
+  baseURL: 'https://graph.facebook.com/v2.9/',
+  timeout: 60000
+})

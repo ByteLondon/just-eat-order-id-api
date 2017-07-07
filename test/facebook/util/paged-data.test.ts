7dbe7210462850faf70d5cbfd9c18c19e9b552a8
Neats29 <anita.amini@protonmail.com>
2017-07-07 18:28:57 +0100

    Added the since data as a variable in tests

--- test/facebook/util/paged-data.test.ts
+++ test/facebook/util/paged-data.test.ts
@@ -5,10 +5,11 @@ import insights from './insights-data.test'
 import adCreativeIds from './adcreativeIds-data.test'
 
 describe('It should paginate:', () => {
+  const since = '2017-07-04'
   it('insight data correctly', () => {
-    expect(determinePagination(insights, '2017-07-04')).to.be.true
+    expect(determinePagination(insights, since)).to.be.true
   })
   it('adcreative data correctly', () => {
-    expect(determinePagination(adCreativeIds, '2017-07-04')).to.be.true
+    expect(determinePagination(adCreativeIds, since)).to.be.true
   })
 })

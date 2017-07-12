096a72dbf54e63f5393d42b16b16938757b1e17a
Neats29 <anita.amini@protonmail.com>
2017-07-12 15:57:47 +0100

    Data is now correctly being exported out as csv

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { fetchInsights } from './facebook/insights'
 import { fetchPosts } from './facebook/posts'
 import { fetchCreatives, fetchCreativeId } from './facebook/creatives'
 import * as Creatives from './model/facebook-creatives'
+import { report } from './report'
 import * as async from 'async'
 import { forIn } from 'lodash'
 
@@ -61,6 +62,4 @@ const insights = (params: Params) => {
   })
 }
 
-// insights(params)
-// posts(params)
-creatives(params)
+report()
---
+++ src/report.ts
@@ -0,0 +1,49 @@
+import * as QueryStream from 'pg-query-stream'
+import { stringify } from 'csv'
+import { createWriteStream } from 'fs'
+import { connect } from './model/core'
+
+export const JOIN = `select
+  insights.ad_id, insights.ad_name, insights.adset_id, insights.adset_name, insights.campaign_id, insights.campaign_name, insights.objective, insights.ad_account,
+  posts.post_id, posts.message, posts.permalink_url, posts.link, posts.type
+  from facebook_insights insights full outer join facebook_creatives creatives on insights.ad_id = creatives.ad_id
+  full outer join facebook_posts posts on creatives.post_id = posts.post_id;`
+
+// export const JOIN = `select
+//   insights.ad_id, insights.ad_name, insights.adset_id, insights.adset_name, insights.campaign_id, insights.campaign_name, insights.objective, insights.ad_account,
+//   posts.post_id, posts.message, posts.permalink_url, posts.link, posts.type
+//   from facebook_insights insights left join facebook_creatives creatives on insights.ad_id = creatives.ad_id
+//   right join facebook_posts posts on creatives.post_id = posts.post_id;`
+
+const toCSV = () =>
+  stringify({
+    columns: [
+      'ad_id',
+      'ad_name',
+      'adset_name',
+      'adset_id',
+      'campaign_id',
+      'campaign_name',
+      'objective',
+      'ad_account',
+      'post_id',
+      'message',
+      'permalink_url',
+      'link',
+      'type'
+    ],
+    header: true
+  })
+
+export const report = () => {
+  connect((err, client, done) => {
+    if (err) throw err
+    const query = new QueryStream(JOIN)
+    const stream = client.query(query)
+    //release the client when the stream is finished
+    stream.on('end', done)
+    stream
+      .pipe(toCSV())
+      .pipe(createWriteStream(`posts3.csv`), { defaultEncoding: 'utf-8' })
+  })
+}

d95df8a6371309d9fd185674cca7d44ae71cc461
Neats29 <anita.amini@protonmail.com>
2017-07-17 11:59:17 +0100

    Removed entry_data and entry_update_date from the tables as it's no longer needed

--- migrations/sqls/20170711105237-create-facebook-insights-table-down.sql
+++ migrations/sqls/20170711105237-create-facebook-insights-table-down.sql
@@ -1,5 +1,1 @@
-drop trigger facebook_insights_entry_update_date on facebook_insights;
-
-drop function entry_update_date();
-
 drop table facebook_insights;
--- migrations/sqls/20170711105711-create-facebook-posts-table-up.sql
+++ migrations/sqls/20170711105711-create-facebook-posts-table-up.sql
@@ -6,12 +6,5 @@ create table facebook_posts
   type text,
   created_time timestamptz,
   link text,
-  page_id text,
-  entry_date timestamp default now() not null,
-  entry_update_date timestamp default now() not null
+  page_id text
 );
-
-
-create trigger facebook_posts_entry_update_date
-before update on facebook_posts 
-for each row execute procedure entry_update_date();

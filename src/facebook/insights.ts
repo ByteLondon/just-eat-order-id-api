311974cb6b2d7302ff79f2e79bc23db5c07a463d
Ian Phillips <ianp@ianp.org>
2017-09-13 17:05:12 +0100

    Upgrade dependencies

--- src/facebook/insights.ts
+++ src/facebook/insights.ts
@@ -1,7 +1,4 @@
-import api, { checkStatusCode } from './util/api'
 import { Insight } from './types/insights'
-import * as config from '../config'
-import * as qs from 'querystring'
 import { fetchPagedData } from './util/paged-data'
 
 //https://developers.facebook.com/docs/marketing-api/insights/fields/v2.9
@@ -16,8 +13,7 @@ export const fetchInsights = async (
     params: {
       access_token: accessToken,
       level: 'ad',
-      fields:
-        'ad_id,ad_name,adset_name,adset_id,campaign_name,campaign_id,objective',
+      fields: 'ad_id,ad_name,adset_name,adset_id,campaign_name,campaign_id,objective',
       time_range: { since, until }
     }
   }
--- src/scrub-order-data.ts
+++ src/scrub-order-data.ts
@@ -4,17 +4,23 @@ import { Transform } from 'stream'
 const postcodeRE = /(?:([a-z]{2}\d[a-z])\s*(\d[a-z]{2}))|(?:([a-z]\d[a-z])\s*(\d[a-z]{2}))|(?:([a-z]\d)\s*(\d[a-z]{2}))|(?:([a-z]\d{2})\s*(\d[a-z]{2}))|(?:([a-z]{2}\d)\s*(\d[a-z]{2}))|(?:([a-z]{2}\d{2})\s*(\d[a-z]{2}))|(?:([a-z]{1,2}\d[a-z]))|(?:([a-z]\d\d?))|(?:([a-z]{2}\d\d?))/i
 
 class Scrubber extends Transform {
+  discarded: number
   constructor() {
     super({ objectMode: true })
+    this.discarded = 0
   }
   _transform(chunk: Array<any>, encoding, cb) {
     // if the row is too short discard it
-    if (chunk.length < 10) return cb(null)
+    if (chunk.filter(a => a.length !== 0).length < 10) {
+      ++this.discarded
+      return cb(null)
+    }
 
     // if it's too long assume that the extra fields are all adress info and smuch them together
-    const scrubbed = chunk.length === 10
-      ? chunk
-      : [...chunk.slice(0, 5), chunk.slice(5, chunk.length - 4).join(' '), ...chunk.slice(chunk.length - 4)]
+    const scrubbed =
+      chunk.length === 10
+        ? chunk
+        : [...chunk.slice(0, 5), chunk.slice(5, chunk.length - 4).join(' '), ...chunk.slice(chunk.length - 4)]
 
     // then look for postcodes and normalize them
     const match = postcodeRE.exec(scrubbed[5])
@@ -25,8 +31,11 @@ class Scrubber extends Transform {
 
 // use this by piping the data through it
 
+const scrubber = new Scrubber()
+
 process.stdin
   .pipe(parse({ relax: true, relax_column_count: true }))
-  .pipe(new Scrubber())
+  .pipe(scrubber)
   .pipe(stringify())
   .pipe(process.stdout)
+  .on('close', () => console.log(`discarded ${scrubber.discarded} records`))
